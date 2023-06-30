3d58fb78a47cdcbf4dd793082127bc6351e1e845
amiel <amielnoy@gmail.com>
2023-06-30 15:36:27 +0300

    add on RssAdapter a click event to open rss article link in cellular browser

--- app/src/main/java/com/example/rss_maariv_pplication/RssAdapter.kt
+++ app/src/main/java/com/example/rss_maariv_pplication/RssAdapter.kt
@@ -1,28 +1,22 @@
-package com.example.rss_maariv_pplication;
+package com.example.rss_maariv_pplication
 
-import android.R.attr.data
+import android.content.Intent
+import android.net.Uri
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
 import android.widget.TextView
 import androidx.recyclerview.widget.RecyclerView
 import com.example.myapplication.R
-import java.util.Date
-
 
 class RssAdapter(private val rssItems: List<RssItem>) :
         RecyclerView.Adapter<RssAdapter.RssViewHolder>() {
-        var rssFeedItems: ArrayList<RssItem> = ArrayList<RssItem>()
-
-        private fun <T> List(): List<RssItem> {
-                TODO("Not yet implemented")
-        }
 
         class RssViewHolder(itemView: View) : RecyclerView.ViewHolder(itemView) {
                 val textViewTitle: TextView = itemView.findViewById(R.id.text_view_title)
                 val textViewLink: TextView = itemView.findViewById(R.id.text_view_link)
-                val textViewAuthor: TextView=itemView.findViewById(R.id.text_view_author)
-                val textViewPubDate: TextView=itemView.findViewById(R.id.text_view_publication_date)
+                val textViewAuthor: TextView = itemView.findViewById(R.id.text_view_author)
+                val textViewPubDate: TextView = itemView.findViewById(R.id.text_view_publication_date)
         }
 
         override fun onCreateViewHolder(parent: ViewGroup, viewType: Int): RssViewHolder {
@@ -35,18 +29,18 @@ class RssAdapter(private val rssItems: List<RssItem>) :
                 val currentItem = rssItems[position]
                 holder.textViewTitle.text = currentItem.title
                 holder.textViewLink.text = currentItem.link
-                holder.textViewAuthor.text=currentItem.author
-                holder.textViewPubDate.text=currentItem.pubDate.toString()
+                holder.textViewAuthor.text = currentItem.author
+                holder.textViewPubDate.text = currentItem.pubDate.toString()
+
+                holder.itemView.setOnClickListener {
+                        val item = rssItems[position]
+                        val intent = Intent(Intent.ACTION_VIEW)
+                        intent.data = Uri.parse(item.link)
+                        holder.itemView.context.startActivity(intent)
+                }
         }
 
         override fun getItemCount(): Int {
                 return rssItems.size
-        }
-
-        fun update(FeedItemsList: ArrayList<RssItem>) {
-                rssFeedItems.clear()
-                rssFeedItems.addAll(FeedItemsList)
-
-                notifyDataSetChanged()
         }
 }
